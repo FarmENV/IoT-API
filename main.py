213476eada46832f8f3e3cd9d26f3806a2742861
Saul Uriel <sauluriel1402@gmail.com>
2021-07-14 17:43:39 -0600

    Inserting data automatically

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_restful import Api
+from werkzeug.wrappers import response
 import db_config as database
-
-from res.measurement import Measurement
+from bson.json_util import ObjectId
 
 app = Flask(__name__)
 api=Api(app)
 CORS(app)
 
-hash_password = "ECF389B6DDCF5D10D1D494924300A2ADC23AFAFD03FE9669C27774ED2895B6EC"
+@app.route("/option/", methods=['GET'])
+def insert():
+    arduinoId = request.args.get("arduinoId")
+    temp = request.args.get("temp")
+    food = request.args.get("food")
+    airQuality = request.args.get("airQuality")
 
-@app.route("/option/<string:_hash>/", methods=['GET'])
-def option(_hash):
-    if _hash == hash_password:
-        arduinoId = request.args.get("arduinoId")
-        temp = request.args.get("temp")
-        food = request.args.get("food")
-        airQuality = request.args.get("airQuality")
+    response = str(database.db.db_iot.insert_one(
+            {
+                'arduinoId':arduinoId,
+                'airQuality':airQuality,
+                'food':food,
+                'temp':temp,
+            }
+        ).inserted_id)
 
-        return jsonify({"arduinoId":arduinoId,"temp":temp,"food":food,"airQuality":airQuality})
+    if response:
+        return jsonify({"response":response})
     else:
-        return jsonify({"500":"error"})
-
-api.add_resource(Measurement,'/new/','/<string:by>:<string:data>/')
+        return jsonify({'message':'Error insert'})
 
 if __name__ == '__main__':
     app.run(load_dotenv=True)
